28665d8a370a36f2238451b81453717a22c618b9
Aleksandar Minchev <aminchev1988@gmail.com>
2025-02-11 17:32:18 +0200

    Install and setup Mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import routes from './routes.js';
 import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
+
+import routes from './routes.js';
 
 const app = express();
+
+try{
+    const uri = 'mongodb://localhost:27017/home-cooking-recipes';
+    await mongoose.connect(uri);
+
+    console.log('DB connected successfully'); 
+
+} catch (err){
+    console.error('Cannot connect to DB');
+    console.log(err.message);    
+}
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
