23b9c41abd6d9747fdd979bdcde0f93cbab720ce
Aleksandar Minchev <aminchev1988@gmail.com>
2025-02-11 17:11:36 +0200

    Install and setup Handlebars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import routes from './routes.js';
+import handlebars from 'express-handlebars';
 
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 
 app.use(express.static('src/public'));
 app.use(express.urlencoded({extended: false}));
